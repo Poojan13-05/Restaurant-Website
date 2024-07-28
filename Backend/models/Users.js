1005840c221d945523001b729334a353538fa05f
Poojan Prajapati <panthprajapati1305@gmail.com>
2024-07-29 01:23:23 +0530

    error solved

--- Backend/models/Users.js
+++ Backend/models/Users.js
@@ -1,31 +1,23 @@
-const mongoose=require("mongoose")
-const passwordcheck = /^(?=.*[0-9])(?=.*[!@#$%^&*])/;
+const mongoose = require("mongoose")
 
-const UserSchema=mongoose.Schema({
-    username:{
-        type:String,
-        min:3,
-        max:20,
-        required:true,
+
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        min :3,
+        max: 20,
+        required: true,
     },
-    password:{
-        type:String,
-        required:true,
-        minlength:[10,"Password must be atleast 10 character long"],
-        validate:{
-            validator:function(value){
-                return passwordcheck.test(value)
-        },
-        message:"Password must contain atleast one number and one special character"
-
-
-    }
-},
-    email:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true,
+        min:10
+    },
+    email: {
+        type: String,
+        required: true,
     }
 
-    
+
 })
-module.exports=mongoose.model("User",UserSchema);
+module.exports = mongoose.model("User", UserSchema);
