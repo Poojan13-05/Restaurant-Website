223949f2b40c650734f18a4a607bad836894d3cc
Poojan Prajapati <panthprajapati1305@gmail.com>
2024-07-28 20:49:51 +0530

    Order backend

---
+++ Backend/models/Order.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose")
+
+const orderSchema= new mongoose.Schema({
+    items:[{
+        itemid:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"MenuItem",
+            required:true
+        },
+        quantity:{
+            type:Number,
+            required:true,
+            min:1
+        }
+        
+    }],
+    totalPrice:Number,
+    customerDetails:{
+        userId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        },
+        name:{
+            type:String,
+            required:true
+        },
+        address:{
+            type:String,
+            required:true
+            
+        },
+        phone:{
+            type:String,
+            required:true
+        }
+
+    },
+    orderDate:{
+        type:Date,
+        default:Date.now
+    }
+});
+module.exports=mongoose.model("Order",orderSchema)
---
+++ Backend/routes/order.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const User = require("../models/Users");
+const Order = require("../models/Order");
+const MenuItem = require("../models/MenuItem");
+
+router.post("/placeOrder",async(req,res)=>{
+    const{userId,name,address,phone,items}=req.body;
+
+    try{
+        const user = await User.findById(userId);
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+
+        let totalPrice=0;
+        for(const item of items){
+            const menuItem = await MenuItem.findById(item.itemId);
+            if (!menuItem){
+                return res.status(404).json({ message:"Menu item not found:${item.itemId}"});
+            }
+            totalPrice+=menuItem.price*item.quantity
+                
+        }
+        const newOrder= new Order({
+            items,
+            totalPrice,
+            customerDetails:{
+                userId,
+                name,
+                address,
+                phone
+
+            }
+        });
+        const savedOrder = await newOrder.save();
+        res.status(201).json(savedOrder);
+    }
+    catch(err){
+        res.status(500).json({message:"Internal Server Error"})
+    }
+
+
+});
+module.exports=router;
+
+
+
+
+
