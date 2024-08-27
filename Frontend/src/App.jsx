de463a3f186c924228b3bf9e90ac7470bf8d226a
Jasmeen2909 <jasmeenkaur2903@gmail.com>
2024-08-28 00:06:55 +0530

    navigation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import Login from './components/Login'
-import Home from './components/Home'
-import Contact from './components/Contact'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
-    <div>
-      <Contact></Contact>
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* <Route path="/order" element={<Order />} />
+        <Route path="/about" element={<About />} /> */}
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import "./Navbar.css"
+import React from "react";
+import { Link } from 'react-router-dom';
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className='nav_main'>
-      <div className='left_lg'>
-        <p className='name'>EasyDine.</p>
+    <div className="nav_main">
+      <div className="left_lg">
+        <p className="name">EasyDine.</p>
       </div>
 
-      <div className='nav_links'>
-        <a href='#'>Home</a>
-        <a href='#'>Order</a>
-        <a href='#'>About</a>
-        <a href='#'>Contact</a>
-        <a href='#'>Login</a>
+      <div className="nav_links">
+        <Link to="/" className="links">Home</Link>
+        <Link to="/order" className="links">Order</Link>
+        <Link to="/about" className="links">About</Link>
+        <Link to="/contact" className="links">Contact</Link>
+        <Link to="/login" className="links">Login</Link>
       </div>
 
-      <div className='right_btn'>
+      <div className="right_btn">
         <button>BOOK A TABLE</button>
-
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
