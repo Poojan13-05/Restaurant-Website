393415fd84cf46ce8f4d58a5ce1c29f76b362a37
Poojan Prajapati <127987352+Poojan13-05@users.noreply.github.com>
2024-09-11 00:50:07 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,7 +9,7 @@ import axios from "axios";
 
 
 const App = () => {
-  const API_URL = process.env.REACT_APP_API_URL;
+  
  
   return (
 
@@ -25,10 +25,6 @@ const App = () => {
   );
 };
 
-export async function fetchData(endpoint) {
-  const response = await fetch(`${API_URL}/${endpoint}`);
-  const data = await response.json();
-  return data;
-}
+
 
 export default App;
